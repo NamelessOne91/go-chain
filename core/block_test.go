df42bb8fe8f5fe6e572e7d83c7768b870bf93fd6
= <=>
2023-06-24 14:17:19 +0200

    Add Block encode/decode unit test

--- core/block_test.go
+++ core/block_test.go
@@ -25,3 +25,23 @@ func TestHeader_Encode_Decode(t *testing.T) {
 	assert.Nil(t, hDecode.DecodeBinary(buf))
 	assert.Equal(t, h, hDecode)
 }
+
+func TestBlock_Encode_Decode(t *testing.T) {
+	b := &Block{
+		Header: Header{
+			Version:   1,
+			PrevBlock: types.RandomHash(),
+			Timestamp: uint64(time.Now().UnixNano()),
+			Height:    10,
+			Nonce:     937868423,
+		},
+		Transactions: nil,
+	}
+
+	buf := &bytes.Buffer{}
+	assert.Nil(t, b.EncodeBinary(buf))
+
+	bDecode := &Block{}
+	assert.Nil(t, bDecode.DecodeBinary(buf))
+	assert.Equal(t, b, bDecode)
+}
