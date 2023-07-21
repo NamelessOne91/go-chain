76447faeab0c9c18aa8aabb137a1c55424d1951e
= <=>
2023-07-21 22:47:53 +0200

    Add Blockchain unit tests

---
+++ core/blockchain_test.go
@@ -0,0 +1,41 @@
+package core
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func newBlockchainWithGenesis(t *testing.T) *Blockchain {
+	bc, err := NewBlockchain(randomBlock(0))
+	assert.Nil(t, err)
+
+	return bc
+}
+
+func TestNewBlockchain(t *testing.T) {
+	bc := newBlockchainWithGenesis(t)
+
+	assert.NotNil(t, bc.validator)
+	assert.Equal(t, bc.Height(), uint32(0))
+}
+
+func TestHasBlock(t *testing.T) {
+	bc := newBlockchainWithGenesis(t)
+	assert.True(t, bc.HasBlock(0))
+}
+
+func TestAddBlock(t *testing.T) {
+	bc := newBlockchainWithGenesis(t)
+
+	lenBlock := 1000
+	for i := 0; i < lenBlock; i++ {
+		block := randomBlockWithSignature(t, uint32(i+1))
+		assert.Nil(t, bc.AddBlock(block))
+	}
+
+	assert.Equal(t, bc.Height(), uint32(lenBlock))
+	assert.Equal(t, len(bc.headers), lenBlock+1)
+
+	assert.NotNil(t, bc.AddBlock(randomBlock(89)))
+}
